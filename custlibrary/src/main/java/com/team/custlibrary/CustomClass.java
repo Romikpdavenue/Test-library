e5738d938a797f0f8138af543efe285b27efd614
Romikpdavenue <romikpdavenue@gmail.com>
2022-11-10 16:45:21 +0530

    Make Static

--- custlibrary/src/main/java/com/team/custlibrary/CustomClass.java
+++ custlibrary/src/main/java/com/team/custlibrary/CustomClass.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class CustomClass {
-    public void see(Context c, String message){
+    public static void seeToast(Context c, String message){
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
     }
 }
