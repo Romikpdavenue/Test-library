0667a811a5a602bf78f67faf853bad857bf6c8c9
Romikpdavenue <romikpdavenue@gmail.com>
2022-11-10 12:20:49 +0530

    first commit

--- custlibrary/src/main/java/com/team/custlibrary/CustomClass.java
+++ custlibrary/src/main/java/com/team/custlibrary/CustomClass.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class CustomClass {
-    public static void s(Context c, String message){
-
+    public void see(Context c, String message){
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
-
     }
 }
